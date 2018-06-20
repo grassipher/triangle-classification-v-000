f8307e096dd75cd7b51abf3487e3be3b431a414a
Gabriel Valle <gvalle91@gmail.com>
2018-06-20 02:14:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,12 @@
 class Triangle
   # write code here
   
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
   class TriangleError < StandardError
     def message
       puts "That is not a valid triangle."
