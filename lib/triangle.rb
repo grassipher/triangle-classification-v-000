f969981dd393e6319594735b6246eb25a126cacd
Gabriel Valle <gvalle91@gmail.com>
2018-06-20 02:16:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,13 @@ class Triangle
     @side3 = side3
   end
   
+  def kind
+    if @side1 == @side2 && @side1 == @side3
+      return :equilateral
+    elsif
+    end
+  end
+  
   class TriangleError < StandardError
     def message
       puts "That is not a valid triangle."
