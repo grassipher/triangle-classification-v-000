74249ea3cf51995d55c5ed53d873cb527e36e97d
Gabriel Valle <gvalle91@gmail.com>
2018-06-20 02:23:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,7 +24,7 @@ class Triangle
   end
   
   def valid?
-    if @side1 <= 0 || @side2 <= 0 || @side3 <=0
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       return false
     elsif (@side1 + @side2) <= @side3 || (@side1 + @side3) <= @side2 || (@side3 + @side2) <= @side1
       return false
